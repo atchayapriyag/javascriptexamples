53c14ac60ad2b02bd28e08f45e9cee4d0eddfc56
atchayapriyag <atchayapriya3333@gmail.com>
2024-04-14 20:57:49 +0530

    javascriptex

---
+++ script.js
@@ -0,0 +1,49 @@
+    
+      // Variables and Types:
+      let userAge = 25;
+      let userName = "sugithaswa";
+      console.log("User Age:", userAge);
+      console.log("User Name:", userName);
+
+      // Comments in JavaScript:
+      /*
+        This function calculates the area of a rectangle
+        based on its width and height.
+        */
+
+      // Operations:
+      let num1 = 20;
+      let num2 = 5;
+      console.log("Addition:", num1 + num2);
+      console.log("Subtraction:", num1 - num2);
+      console.log("Multiplication:", num1 * num2);
+      console.log("Division:", num1 / num2);
+
+      // Data Types:
+      let stringExample = "Hello, World!";
+      let numberExample = 55;
+      let booleanExample = true;
+      let arrayExample = [1, 2, 3, 4, 5];
+
+      // Functions in JavaScript:
+      function greetUser(name) {
+        return "Hello, " + name + "!";
+      }
+      console.log(greetUser("Aswa"));
+
+      // if Else in JavaScript:
+      let temperature = 15;
+      if (temperature > 20) {
+        console.log("It's hot outside!");
+      } else {
+        console.log("It's not too hot outside.");
+      }
+
+      // FOR LOOP:
+      for (let i = 1; i <= 5; i++) {
+        console.log(i);
+      }
+
+      // Loose vs Strict Equality:
+      console.log(5 == "5"); // true (loose equality)
+      console.log(5 === "5"); // false (strict equality)
